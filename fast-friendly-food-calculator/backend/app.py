cd2b639e984f6ce13a1bdf4114a02f401d0cf3a4
Justin <justin022306@gmail.com>
2025-10-04 15:18:48 -0400

    idk

--- fast-friendly-food-calculator/backend/app.py
+++ fast-friendly-food-calculator/backend/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify
-from routes import init_routes
+from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fast_friendly_food.db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def home():
-    return "Welcome to the Fast Friendly Food Calculator API!"
+    return "Welcome to the Fast Friendly Food Calculator!"
 
 if __name__ == '__main__':
-    init_routes(app)
     app.run(debug=True)
---
+++ fast-friendly-food-calculator/backend/extensions.py
@@ -0,0 +1,5 @@
+# extentions.py
+from flask_sqlalchemy import SQLAlchemy
+
+# Single SQLAlchemy instance to be shared across app/models
+db = SQLAlchemy()
